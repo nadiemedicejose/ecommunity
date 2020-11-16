fdbaad7bdf9f80805c34e8cc70ebbcb8e37279e3
Jose Estrada <44657605+namestartswithj@users.noreply.github.com>
2020-11-15 23:35:47 -0700

    Jose (#2)
    * Agrego pantalla de Comunidades
    
    * Pantalla de Registro
    
    * Se agregan campos a pantalla de registro

--- lib/src/view/registro.dart
+++ lib/src/view/registro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class Registro extends StatefulWidget {
   @override
@@ -6,6 +7,26 @@ class Registro extends StatefulWidget {
 }
 
 class _RegistroState extends State<Registro> {
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+  void validate() {
+    if (formkey.currentState.validate()) {
+      print("Validado");
+    } else {
+      print("No validado");
+    }
+  }
+
+  String validarContrasena(value) {
+    if (value.isEmpty) {
+      return "Especifica una contraseña";
+    } else if (value.length < 6) {
+      return "Al menos 6 caracteres";
+    } else {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +34,95 @@ class _RegistroState extends State<Registro> {
         leading: BackButton(),
         title: Text("Registro"),
       ),
+      body: SingleChildScrollView(
+          padding: EdgeInsets.all(25.0),
+          child: Form(
+            autovalidate: true,
+            key: formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Nombre",
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 20.0,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Email",
+                    ),
+                    validator: MultiValidator([
+                      RequiredValidator(errorText: "Requerido"),
+                      EmailValidator(errorText: "Correo no válido")
+                    ]),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 20.0,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Teléfono",
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 20.0,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Residencia",
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 20.0,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Contraseña",
+                    ),
+                    validator: validarContrasena,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 20.0,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Confirmar contraseña",
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 20.0,
+                  ),
+                  child: RaisedButton(
+                    child: Text("Registrarme"),
+                    onPressed: validate,
+                    shape: StadiumBorder(),
+                    color: Colors.green,
+                    textColor: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          )),
       body: Padding(
         padding: EdgeInsets.all(25.0),
         child: Form(
