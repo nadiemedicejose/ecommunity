87820a408c76193247fbaf42258aa31b5b32d593
Jose Estrada <j.estrada49@icloud.com>
2020-11-15 22:04:35 -0700

    Pantalla de Registro

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,19 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:ecommunity/src/view/principal.dart';
 import 'package:ecommunity/src/view/otrapantalla.dart';
 import 'package:ecommunity/src/widgets/botones.dart';
+import 'package:ecommunity/src/view/registro.dart';
 
 class ECOmmunity extends StatelessWidget {
   // Este widget es la raíz de la aplicación.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ECOmmunity',
       theme: ThemeData(
         // Define el tema de la aplicación
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Botones(),
+      home: Registro(),
     );
   }
 } 
