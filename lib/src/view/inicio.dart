19685fbe477b43525ed0db7db426dd7add93a59e
Jose Estrada <j.estrada49@icloud.com>
2020-11-19 19:35:48 -0700

    Agregar X cantidad de comunidades (avatars)

--- lib/src/view/inicio.dart
+++ lib/src/view/inicio.dart
@@ -121,6 +121,20 @@ class _InicioState extends State<Inicio> {
   }
 }
 
+List<Widget> _communities() {
+  List<Widget> comunidades = [];
+  final int total = 5;
+  for (int i = 0; i < total; i++) {
+    if (i < total-1) {
+      comunidades.add(AvatarComunidad());
+      comunidades.add(SizedBox(width: 5));
+    } else {
+      comunidades.add(AvatarComunidad());
+    }
+  }
+  return comunidades;
+}
+
 class Comunidades extends StatelessWidget {
   const Comunidades({
     Key key,
@@ -133,27 +147,7 @@ class Comunidades extends StatelessWidget {
       height: 100.0,
       child: ListView(
         scrollDirection: Axis.horizontal,
-        children: <Widget>[
-          AvatarComunidad(
-            color: Colors.red,
-          ),
-          SizedBox(width: 5),
-          AvatarComunidad(
-            color: Colors.blue,
-          ),
-          SizedBox(width: 5),
-          AvatarComunidad(
-            color: Colors.green,
-          ),
-          SizedBox(width: 5),
-          AvatarComunidad(
-            color: Colors.yellow,
-          ),
-          SizedBox(width: 5),
-          AvatarComunidad(
-            color: Colors.orange,
-          ),
-        ],
+        children: _communities(),
       ),
     );
   }
